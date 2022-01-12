48a5d3780551d9f2a2ce24505116414ce100d099
p-nuts5963 <polythene.pam5963@gimail.com>
2022-01-12 23:43:35 +0900

    add app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,24 @@
 module.exports = {
     siteMetadata: {
       title: `My Gatsby Site`,
-        siteUrl: `https://www.yourdomain.tld`,
+      desvription: "Hello Gatsby"
     },
     plugins: [
-
+      {
+        resolve: `gatsby-source-rss-feed`,
+        options: {
+          url: `https://qiita.com/mottox2/feed`,
+          name: `Qiita`
+        }
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: "works",
+          path: `${__dirname}/src/works.yaml`
+        }
+      },
+      'gatsby-transformer-yaml',
+      "gatsby-plugin-react-helmet"
     ]
 }
